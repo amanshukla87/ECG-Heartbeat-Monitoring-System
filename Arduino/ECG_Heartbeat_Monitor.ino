b580e4ecdbab5939eb2fa3d44ff840cd757a6098
Aman Shukla <109803350+amanshukla87@users.noreply.github.com>
2026-09-11 15:32:15 +0530

    Clean up ECG firmware pin definitions

--- Arduino/ECG_Heartbeat_Monitor.ino
+++ Arduino/ECG_Heartbeat_Monitor.ino
@@ -1,50 +1,51 @@
 // Arduino-Based ECG Heartbeat Monitoring Prototype
 // Arduino UNO + AD8232 ECG sensor
 
-const int ecgPin = A0;
-const int leadOffPlus = 10;
-const int leadOffMinus = 11;
+const byte ECG_PIN = A0;
+const byte LEAD_OFF_PLUS = 10;
+const byte LEAD_OFF_MINUS = 11;
 
-const int greenLed = 6;
-const int redLed = 7;
-const int buzzer = 8;
+const byte GREEN_LED = 6;
+const byte RED_LED = 7;
+const byte BUZZER = 8;
 
 void setup()
 {
   Serial.begin(9600);
 
-  pinMode(leadOffPlus, INPUT);
-  pinMode(leadOffMinus, INPUT);
+  pinMode(LEAD_OFF_PLUS, INPUT);
+  pinMode(LEAD_OFF_MINUS, INPUT);
 
-  pinMode(greenLed, OUTPUT);
-  pinMode(redLed, OUTPUT);
-  pinMode(buzzer, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 
-  digitalWrite(greenLed, LOW);
-  digitalWrite(redLed, LOW);
-  digitalWrite(buzzer, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(BUZZER, LOW);
 }
 
 void loop()
 {
-  int ecgValue = analogRead(ecgPin);
+  const int ecgValue = analogRead(ECG_PIN);
+  const bool leadOff = (digitalRead(LEAD_OFF_PLUS) == HIGH) ||
+                       (digitalRead(LEAD_OFF_MINUS) == HIGH);
 
-  // AD8232 indicates a disconnected electrode through LO+ or LO-.
-  if (digitalRead(leadOffPlus) == HIGH || digitalRead(leadOffMinus) == HIGH)
+  if (leadOff)
   {
-    digitalWrite(greenLed, LOW);
-    digitalWrite(redLed, HIGH);
-    digitalWrite(buzzer, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(BUZZER, HIGH);
 
     Serial.println("Lead off");
   }
   else
   {
-    digitalWrite(greenLed, HIGH);
-    digitalWrite(redLed, LOW);
-    digitalWrite(buzzer, LOW);
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BUZZER, LOW);
 
-    // Send the ECG signal to the Serial Plotter.
+    // Send the ECG sample to the Arduino Serial Plotter.
     Serial.println(ecgValue);
   }
 
